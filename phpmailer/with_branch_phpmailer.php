e79943f9b914d32910d177b0cb2dd04b6c8184ff
Mohamed Fezaa <76229378+mohamed97fezaa@users.noreply.github.com>
2023-10-07 15:41:54 +0200

    Add files via upload

---
+++ phpmailer/with_branch_phpmailer.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Ansonika">
+    <title>Magnifica | Corona Virus Covid-19 Questionnaire Form Wizard by Ansonika</title>
+
+    <!-- GOOGLE WEB FONT -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+    <!-- YOUR CUSTOM CSS -->
+    <link href="../css/custom.css" rel="stylesheet">
+    
+	<script type="text/javascript">
+    function delayedRedirect(){
+        window.location = "../index-2.html"
+    }
+    </script>
+
+</head>
+<body onLoad="setTimeout('delayedRedirect()', 8000)" style="background-color:#fff;">
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+
+try {
+
+    //Recipients - main edits
+    $mail->setFrom('info@magnifica.com', 'Message from MAGNIFICA');                // Email Address and Name FROM
+    $mail->addAddress('jhon@magnifica.com', 'Jhon Doe');                           // Email Address and Name TO - Name is optional
+    $mail->addReplyTo('noreply@magnifica.com', 'Message from MAGNIFICA');          // Email Address and Name NOREPLY
+    $mail->isHTML(true);                                                       
+    $mail->Subject = 'Message from MAGNIFICA';                                     // Email Subject
+
+    //The email body message
+    $message  = "<strong>Questionnaire</strong><br />";
+    $message .= "Have you traveled to any one of the destinations below in the last 21 days?<br />" ;
+        foreach($_POST['question_1'] as $value) 
+        { 
+            $message .=   "- " .  trim(stripslashes($value)) . "<br />"; 
+        };
+
+    $message .= "<br />Have you recently been in contact with a person with Coronavirus? " . $_POST['question_2'];
+    $message .= "<br />Are you experiencing any difficulty in breathing? " . $_POST['question_3'];
+    if (isset($_POST['question_4']) && $_POST['question_4'] != "")
+        {
+        $message .= "<br />Please tick any one of the following symptoms that can be applies to you:<br />";
+        foreach($_POST['question_4'] as $value)
+            {
+                $message.= "- " . trim(stripslashes($value)) . "<br />";
+            };
+        }
+    
+    $message .= "<br />Do you have fever higher than 100.3° F? " . $_POST['question_5'];
+    $message .= "<br />Do you have a runny nose? " . $_POST['question_6'];
+    $message .= "<br />Are you experiencing muscle aches, weakness, or lightheadedness? " . $_POST['question_7'];
+    $message .= "<br />Are you having diarrhea, stomach pain, vomiting? " . $_POST['question_8'];
+
+    $message .= "<br /><br /><strong>User Details</strong>";
+    $message .= "<br />First and Last Name: " . $_POST['name'];
+    $message .= "<br />Email: " . $_POST['email'];
+    $message .= "<br />Telephone: " . $_POST['phone'];
+    $message .= "<br />Gender: " . $_POST['gender'];
+
+	$mail->Body = "" . $message . "";
+
+    $mail->send();
+
+    // Confirmation/autoreplay email send to who fill the form
+    $mail->ClearAddresses();
+    $mail->addAddress($_POST['email']); // Email address entered on form
+    $mail->isHTML(true);
+    $mail->Subject    = 'Confirmation'; // Custom subject
+    $mail->Body = "" . $message . "";
+
+    $mail->Send();
+
+    echo '<div id="success">
+            <div class="icon icon--order-success svg">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="72px" height="72px">
+                  <g fill="none" stroke="#8EC343" stroke-width="2">
+                     <circle cx="36" cy="36" r="35" style="stroke-dasharray:240px, 240px; stroke-dashoffset: 480px;"></circle>
+                     <path d="M17.417,37.778l9.93,9.909l25.444-25.393" style="stroke-dasharray:50px, 50px; stroke-dashoffset: 0px;"></path>
+                  </g>
+                 </svg>
+             </div>
+            <h4><span>Request successfully sent!</span>Thank you for your time</h4>
+            <small>You will be redirect back in 5 seconds.</small>
+        </div>';
+	} catch (Exception $e) {
+	    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+	}
+	
+?>
+<!-- END SEND MAIL SCRIPT -->   
+
+</body>
+</html>
